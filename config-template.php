38081f75d5c4a43e9631b5aa23bfb02057338883
atchopba <atchopba@gmail.com>
2020-05-09 11:38:49 +0200

    Update config-template.php

--- config-template.php
+++ config-template.php
@@ -4,4 +4,11 @@ define("__host__", "xxxxxxxx");
 define("__user__", "xxxxxxxx");
 define("__pwd__", "xxxxxxxx");
 define("__db__", "xxxxxxxx");
+
+// definition de l'@mail d'envoi
+define("__adrmail_from__", "contact@digex.tech");
+define("__subject_mail__", "Mail de relance - renouvellement du domaine");
+
+// fichier log
+define("__log_file__", "logs/notifier-log.log");
 ?>
